c512d18ed0a2d418e8a368e8d2ef94264200725d
Kazoroo <bczarnecki@duck.com>
2023-11-16 19:11:37 +0100

    Added animated logo on the splash screen.
    Took 1 hour 20 minutes

--- app/src/main/java/pl/kazoroo/dices/MainActivity.kt
+++ app/src/main/java/pl/kazoroo/dices/MainActivity.kt
@@ -1,6 +1,9 @@
 package pl.kazoroo.dices
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -9,14 +12,42 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import pl.kazoroo.dices.data.DicesViewModel
+import pl.kazoroo.dices.data.SplashScreenViewModel
 import pl.kazoroo.dices.navigation.Navigation
 import pl.kazoroo.dices.ui.theme.DicesTheme
 
 class MainActivity : ComponentActivity() {
+    private val splashScreenViewModel by viewModels<SplashScreenViewModel>()
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !splashScreenViewModel.isReady.value
+            }
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                        screen.iconView, View.SCALE_X, 0.4f, 0.0f
+                )
+                zoomX.interpolator = OvershootInterpolator()
+                zoomX.duration = 500L
+                zoomX.doOnEnd { screen.remove() }
+
+                val zoomY = ObjectAnimator.ofFloat(
+                        screen.iconView, View.SCALE_Y, 0.4f, 0.0f
+                )
+                zoomY.interpolator = OvershootInterpolator()
+                zoomY.duration = 500L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
+            }
+        }
         setContent {
             DicesTheme {
                 val viewModel by viewModels<DicesViewModel>()
---
+++ app/src/main/java/pl/kazoroo/dices/data/SplashScreenViewModel.kt
@@ -0,0 +1,20 @@
+package pl.kazoroo.dices.data
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SplashScreenViewModel : ViewModel() {
+    private val _isReady = MutableStateFlow(false)
+    val isReady = _isReady.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(10L)
+            _isReady.value = true
+        }
+    }
+}
