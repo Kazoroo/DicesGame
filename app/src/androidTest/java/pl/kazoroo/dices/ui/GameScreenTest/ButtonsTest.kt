b8487e7a9a2f360101e66619c96aba033f8f9d8a
Kazoroo <kazoroo1903@gmail.com>
2024-09-13 21:23:25 +0200

    Add a results table and the paper_texture.jpg as it's background.

--- app/src/androidTest/java/pl/kazoroo/dices/ui/GameScreenTest/ButtonsTest.kt
+++ app/src/androidTest/java/pl/kazoroo/dices/ui/GameScreenTest/ButtonsTest.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
-import pl.kazoroo.dices.presentation.game.Buttons
 
 class ButtonsTest {
     @get:Rule
@@ -13,13 +12,7 @@ class ButtonsTest {
     @Test
     fun buttonEndQueueIsDisplayed() {
         composeTestRule.setContent {
-            Buttons(
-                    onThrowClick = { },
-                    onQueueClick = { },
-                    weight = 2,
-                    height = 2,
-                    isDiceSelected = listOf(true, true, true, true, true, true)
-            )
+
         }
 
         composeTestRule.onNodeWithText("Confirm and end the queue").assertExists()
@@ -28,13 +21,7 @@ class ButtonsTest {
     @Test
     fun buttonEndRoundIsDisplayed() {
         composeTestRule.setContent {
-            Buttons(
-                    onThrowClick = { },
-                    onQueueClick = { },
-                    weight = 2,
-                    height = 2,
-                    isDiceSelected = listOf(true, true, true, true, true, true)
-            )
+
         }
 
         composeTestRule.onNodeWithText("Confirm and complete the round").assertExists()
