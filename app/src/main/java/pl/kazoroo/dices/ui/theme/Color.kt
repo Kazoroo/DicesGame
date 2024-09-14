fbd58c25b2891ed3f00f2a947d491ff070deffd6
Kazoroo <kazoroo1903@gmail.com>
2024-09-14 20:00:00 +0200

    Add gradient dividers to table rows.

--- app/src/main/java/pl/kazoroo/dices/ui/theme/Color.kt
+++ app/src/main/java/pl/kazoroo/dices/ui/theme/Color.kt
@@ -4,3 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val Purple700 = Color(0xFFAA073E)
 val Teal200 = Color(0xFFc0da3f)
+
+val DarkGoldenBrown = Color(0xFFA17E44)
+val HalfTransparentBlack = Color(0x80000000)
