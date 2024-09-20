fe00eb8a1c98d6de23203564f4ca770fd8cacc9f
Kazoroo <kazoroo1903@gmail.com>
2024-09-20 20:04:07 +0200

    Implement checking for skucha and displaying an information that skucha occurred.

--- app/src/main/java/pl/kazoroo/dices/presentation/game/DicesGameScreen.kt
+++ app/src/main/java/pl/kazoroo/dices/presentation/game/DicesGameScreen.kt
@@ -1,19 +1,32 @@
 package pl.kazoroo.dices.presentation.game
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import pl.kazoroo.dices.R
 import pl.kazoroo.dices.domain.model.SimpleData
 import pl.kazoroo.dices.presentation.components.ButtonInfo
@@ -40,9 +53,10 @@ fun DicesGameScreen(viewModel: DicesViewModel) {
             opponentPoints = "0"
         ),
     )
+    val isSkucha = viewModel.skuchaState.collectAsState().value
 
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
     ) {
         Image(
             painter = painterResource(id = R.drawable.wooden_background_texture),
@@ -76,7 +90,10 @@ fun DicesGameScreen(viewModel: DicesViewModel) {
             val buttonsInfo = listOf(
                 ButtonInfo(
                     text = stringResource(id = R.string.score_and_roll_again),
-                    onClick = { viewModel.countPoints() }
+                    onClick = {
+                        viewModel.countPoints()
+                        viewModel.checkForSkucha()
+                    }
                 ),
                 ButtonInfo(
                     text = stringResource(id = R.string.pass),
@@ -84,6 +101,37 @@ fun DicesGameScreen(viewModel: DicesViewModel) {
                 ),
             )
             GameButtons(buttonsInfo)
+        }
+
+        var showTextWithDelay by remember { mutableStateOf(false) }
+
+        LaunchedEffect(isSkucha) {
+            if (isSkucha) {
+                delay(1000L)
+                showTextWithDelay = true
+                delay(2000L)
+                showTextWithDelay = false
+                //TODO reseting points and new dice set function call
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            if(showTextWithDelay) {
+                Text(
+                    text = "Skucha!",
+                    color = Color(212, 212, 212),
+                    style = MaterialTheme.typography.displayLarge,
+                    modifier = Modifier
+                        .background(
+                            color = Color(26, 26, 26, 220),
+                            shape = RoundedCornerShape(25.dp)
+                        )
+                        .padding(20.dp)
+                )
+            }
         }
     }
 }
