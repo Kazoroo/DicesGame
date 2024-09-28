b6b46eb8741c393527664adf8881046d19bdab63
Kazoroo <kazoroo1903@gmail.com>
2024-09-28 15:41:37 +0200

    Move Exit button in MainMenuScreen to the bottom, make buttons and their texts bigger.

--- app/src/main/java/pl/kazoroo/dices/presentation/mainmenu/DicesMainMenuScreen.kt
+++ app/src/main/java/pl/kazoroo/dices/presentation/mainmenu/DicesMainMenuScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -14,8 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -30,36 +29,42 @@ fun MainMenuScreen(navController: NavController) {
         ButtonInfo(
             text = stringResource(R.string.play_with_ai),
             modifier = Modifier
-                .width(200.dp)
-                .semantics { contentDescription = "Play with AI button" }
+                .width(220.dp)
         ) {
             navController.navigate(Screen.GameScreen.withArgs())
-        },
-        ButtonInfo(
-            text = stringResource(R.string.exit),
-            modifier = Modifier
-                .width(200.dp)
-                .semantics { contentDescription = "Exit button" }
-        ) {
-            activity?.finish()
         }
     )
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.semantics { contentDescription = "Main menu screen" }
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = "Dice")
         Spacer(modifier = Modifier.height(40.dp))
 
         buttons.forEach { buttonInfo ->
             Button(
-                onClick = buttonInfo.onClick, modifier = buttonInfo.modifier
+                onClick = buttonInfo.onClick,
+                modifier = buttonInfo.modifier
             ) {
                 Text(
-                    text = buttonInfo.text, fontSize = 16.sp
+                    text = buttonInfo.text, fontSize = 18.sp
                 )
             }
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Button(
+            onClick = {
+                activity?.finish()
+            },
+            modifier = Modifier
+                .width(220.dp)
+                .padding(bottom = 50.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.exit), fontSize = 18.sp
+            )
         }
     }
 }
