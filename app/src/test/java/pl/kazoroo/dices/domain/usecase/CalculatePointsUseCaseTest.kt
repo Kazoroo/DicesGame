345afcbde4b6344abe3fcf1d7e89cf4ee4b8e9e5
Kazoroo <kazoroo1903@gmail.com>
2024-09-30 19:16:07 +0200

    Create tests for all three UseCase.

---
+++ app/src/test/java/pl/kazoroo/dices/domain/usecase/CalculatePointsUseCaseTest.kt
@@ -0,0 +1,151 @@
+package pl.kazoroo.dices.domain.usecase
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+open class CalculatePointsUseCaseTest {
+
+    @Test
+    fun `check if three 1s gives 1000 points`() {
+        val result = CalculatePointsUseCase().invoke(
+            diceList = listOf(
+                Dice(1, 0),
+                Dice(1, 0),
+                Dice(1, 0),
+                Dice(2, 0),
+                Dice(3, 0),
+                Dice(4, 0),
+            ),
+            isDiceSelected = listOf(
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+            )
+        )
+
+        assertEquals(1000, result)
+    }
+
+    @Test
+    fun `check if game count not selected dices`() {
+        val result = CalculatePointsUseCase().invoke(
+            diceList = listOf(
+                Dice(1, 0),
+                Dice(1, 0),
+                Dice(1, 0),
+                Dice(2, 0),
+                Dice(3, 0),
+                Dice(4, 0),
+            ),
+            isDiceSelected = listOf(
+                false,
+                false,
+                false,
+                true,
+                true,
+                true,
+            )
+        )
+
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun `check if four 1s gives 2000 points`() {
+        val result = CalculatePointsUseCase().invoke(
+            diceList = listOf(
+                Dice(1, 0),
+                Dice(1, 0),
+                Dice(1, 0),
+                Dice(1, 0),
+                Dice(3, 0),
+                Dice(4, 0),
+            ),
+            isDiceSelected = listOf(
+                true,
+                true,
+                true,
+                true,
+                true,
+                true,
+            )
+        )
+
+        assertEquals(2000, result)
+    }
+
+    @Test
+    fun `check if four 3s gives 600 points`() {
+        val result = CalculatePointsUseCase().invoke(
+            diceList = listOf(
+                Dice(3, 0),
+                Dice(3, 0),
+                Dice(3, 0),
+                Dice(1, 0),
+                Dice(3, 0),
+                Dice(4, 0),
+            ),
+            isDiceSelected = listOf(
+                true,
+                true,
+                true,
+                false,
+                false,
+                false,
+            )
+        )
+
+        assertEquals(600, result)
+    }
+
+    @Test
+    fun `check if one 5 gives 50 points`() {
+        val result = CalculatePointsUseCase().invoke(
+            diceList = listOf(
+                Dice(5, 0),
+                Dice(3, 0),
+                Dice(3, 0),
+                Dice(1, 0),
+                Dice(3, 0),
+                Dice(4, 0),
+            ),
+            isDiceSelected = listOf(
+                true,
+                false,
+                false,
+                false,
+                false,
+                false,
+            )
+        )
+
+        assertEquals(50, result)
+    }
+
+    @Test
+    fun `check if two 5 gives 100 points`() {
+        val result = CalculatePointsUseCase().invoke(
+            diceList = listOf(
+                Dice(5, 0),
+                Dice(5, 0),
+                Dice(3, 0),
+                Dice(1, 0),
+                Dice(3, 0),
+                Dice(4, 0),
+            ),
+            isDiceSelected = listOf(
+                true,
+                true,
+                false,
+                false,
+                false,
+                false,
+            )
+        )
+
+        assertEquals(100, result)
+    }
+}
---
+++ app/src/test/java/pl/kazoroo/dices/domain/usecase/DrawDiceUseCaseTest.kt
@@ -0,0 +1,22 @@
+package pl.kazoroo.dices.domain.usecase
+
+import org.junit.Test
+
+class DrawDiceUseCaseTest {
+
+    @Test
+    fun `check if there are 6 dice`() {
+        val result = DrawDiceUseCase().invoke()
+
+        assert(result.size == 6)
+    }
+
+    @Test
+    fun `check if dice values are in appropriate range`() {
+        val result = DrawDiceUseCase().invoke()
+
+        val areValuesInRange = result.map { it.value.coerceIn(1..6) == it.value }
+
+        assert(areValuesInRange.all { it })
+    }
+}
