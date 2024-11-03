09b486b34fec0ad4aa6aa1a617f99551f7c575fa
Kazoroo <kazoroo1903@gmail.com>
2024-11-03 15:36:26 +0100

    Add LazyColumn to HowToPlayDialog so the text isn't cut on smaller phones.

--- app/src/main/java/pl/kazoroo/dices/presentation/mainmenu/components/HowToPlayDialog.kt
+++ app/src/main/java/pl/kazoroo/dices/presentation/mainmenu/components/HowToPlayDialog.kt
@@ -1,12 +1,12 @@
 package pl.kazoroo.dices.presentation.mainmenu.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -31,47 +31,51 @@ fun HowToPlayDialog(
     Dialog(
         onDismissRequest = onClick
     ) {
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .background(
                     color = Color.White,
                     shape = RoundedCornerShape(dimensionResource(R.dimen.medium_padding))
                 )
         ) {
-            Row(
-                modifier = Modifier.padding(
-                    top = dimensionResource(R.dimen.small_padding),
-                    start = dimensionResource(R.dimen.small_padding),
-                    end = dimensionResource(R.dimen.small_padding),
-                ),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "How to play?",
-                    style = MaterialTheme.typography.titleSmall
-                )
-
-                Spacer(Modifier.weight(1f))
-
-                IconButton(
-                    onClick = onClick,
-                    modifier = Modifier
-                        .wrapContentSize()
+            item {
+                Row(
+                    modifier = Modifier.padding(
+                        top = dimensionResource(R.dimen.small_padding),
+                        start = dimensionResource(R.dimen.small_padding),
+                        end = dimensionResource(R.dimen.small_padding),
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = "Info icon",
-                        tint = DarkRed,
-                        modifier = Modifier.fillMaxSize()
+                    Text(
+                        text = "How to play?",
+                        style = MaterialTheme.typography.titleSmall
                     )
+
+                    Spacer(Modifier.weight(1f))
+
+                    IconButton(
+                        onClick = onClick,
+                        modifier = Modifier
+                            .wrapContentSize()
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = "Info icon",
+                            tint = DarkRed,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
                 }
             }
 
-            Text(
-                text = stringResource(R.string.how_to_play_long_tutorial),
-                modifier = Modifier
-                    .padding(dimensionResource(R.dimen.medium_padding))
-            )
+            item {
+                Text(
+                    text = stringResource(R.string.how_to_play_long_tutorial),
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.medium_padding))
+                )
+            }
         }
     }
 }
