597a9e3b8ca66996954079f53cd508ab52425824
Kazoroo <kazoroo1903@gmail.com>
2024-11-10 14:22:12 +0100

    Write tests for UserDataRepository

--- app/src/main/java/pl/kazoroo/dices/core/data/local/UserDataRepository.kt
+++ app/src/main/java/pl/kazoroo/dices/core/data/local/UserDataRepository.kt
@@ -1,28 +1,26 @@
 package pl.kazoroo.dices.core.data.local
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_data")
+//TODO: private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_data")
 
-class UserDataRepository(private val context: Context) {
+class UserDataRepository(private val dataStore: DataStore<Preferences>) {
 
     suspend fun saveNewValue(key: UserDataKey, value: String) {
         val dataStoreKey = stringPreferencesKey(key.name)
 
-        context.dataStore.edit { userData ->
+        dataStore.edit { userData ->
             userData[dataStoreKey] = value
         }
     }
 
     suspend fun readValue(key: UserDataKey): String? {
         val dataStoreKey = stringPreferencesKey(key.name)
-        val preferences = context.dataStore.data.first()
+        val preferences = dataStore.data.first()
 
         return preferences[dataStoreKey]
     }
