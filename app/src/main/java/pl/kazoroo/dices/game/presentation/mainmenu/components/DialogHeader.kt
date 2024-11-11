9cd86c43e94b499baf84054e5f1d455343f4f6ea
Kazoroo <kazoroo1903@gmail.com>
2024-11-11 19:32:59 +0100

    Extract DialogHeader from HowToPlayDialog  separate file, add behaviour of displaying BettingDialog when play button clicked, add basic BettingDialog.

---
+++ app/src/main/java/pl/kazoroo/dices/game/presentation/mainmenu/components/DialogHeader.kt
@@ -0,0 +1,55 @@
+package pl.kazoroo.dices.game.presentation.mainmenu.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import pl.kazoroo.dices.R
+import pl.kazoroo.dices.ui.theme.DarkRed
+
+@Composable
+fun DialogHeader(
+    headerText: String,
+    modifier: Modifier = Modifier,
+    onCloseClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier.padding(
+            top = dimensionResource(R.dimen.small_padding),
+            start = dimensionResource(R.dimen.small_padding),
+            end = dimensionResource(R.dimen.small_padding),
+        ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = headerText,
+            style = MaterialTheme.typography.titleSmall
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        IconButton(
+            onClick = onCloseClick,
+            modifier = Modifier
+                .wrapContentSize()
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "Info icon",
+                tint = DarkRed,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
--- app/src/main/java/pl/kazoroo/dices/game/presentation/mainmenu/components/HowToPlayDialog.kt
+++ app/src/main/java/pl/kazoroo/dices/game/presentation/mainmenu/components/HowToPlayDialog.kt
@@ -1,35 +1,24 @@
 package pl.kazoroo.dices.game.presentation.mainmenu.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.window.Dialog
 import pl.kazoroo.dices.R
-import pl.kazoroo.dices.ui.theme.DarkRed
 
 @Composable
 fun HowToPlayDialog(
-    onClick: () -> Unit
+    onCloseClick: () -> Unit
 ) {
     Dialog(
-        onDismissRequest = onClick
+        onDismissRequest = onCloseClick
     ) {
         LazyColumn(
             modifier = Modifier
@@ -39,33 +28,10 @@ fun HowToPlayDialog(
                 )
         ) {
             item {
-                Row(
-                    modifier = Modifier.padding(
-                        top = dimensionResource(R.dimen.small_padding),
-                        start = dimensionResource(R.dimen.small_padding),
-                        end = dimensionResource(R.dimen.small_padding),
-                    ),
-                    verticalAlignment = Alignment.CenterVertically
+                DialogHeader(
+                    headerText = stringResource(R.string.how_to_play)
                 ) {
-                    Text(
-                        text = "How to play?",
-                        style = MaterialTheme.typography.titleSmall
-                    )
-
-                    Spacer(Modifier.weight(1f))
-
-                    IconButton(
-                        onClick = onClick,
-                        modifier = Modifier
-                            .wrapContentSize()
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Close,
-                            contentDescription = "Info icon",
-                            tint = DarkRed,
-                            modifier = Modifier.fillMaxSize()
-                        )
-                    }
+                    onCloseClick()
                 }
             }
 
