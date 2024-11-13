28b66b35aaf286382f67f9efb059b2a8a36dbdc7
Kazoroo <kazoroo1903@gmail.com>
2024-11-13 20:46:54 +0100

    Implement reading coins amount from DS, modify setBetValue() to substract the bet from the coins amount. Add ReadUserDataUseCase

--- app/src/main/java/pl/kazoroo/dices/core/data/presentation/BettingViewModel.kt
+++ app/src/main/java/pl/kazoroo/dices/core/data/presentation/BettingViewModel.kt
@@ -5,26 +5,40 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import pl.kazoroo.dices.core.domain.ReadUserDataUseCase
 import pl.kazoroo.dices.core.domain.SaveUserDataUseCase
 
 class BettingViewModel(
-    private val saveUserDataUseCase: SaveUserDataUseCase
+    private val saveUserDataUseCase: SaveUserDataUseCase,
+    private val readUserDataUseCase: ReadUserDataUseCase
 ) : ViewModel() {
     private val _betValue = MutableStateFlow("0")
     val betValue = _betValue.asStateFlow()
 
-    private val _coinsAmount = MutableStateFlow(readCoinsAmount())
+    private val _coinsAmount = MutableStateFlow("")
     val coinsAmount = _coinsAmount.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _coinsAmount.value = readCoinsAmount()
+        }
+    }
 
     fun setBetValue(value: String) {
         _betValue.value = value
 
         viewModelScope.launch {
-            saveUserDataUseCase.invoke(value)
+            val coins = readCoinsAmount()
+            val newCoinBalance = (coins.toInt() - value.toInt()).toString()
+            saveUserDataUseCase.invoke(newCoinBalance)
         }
     }
 
-    private fun readCoinsAmount(): String {
-        return ""
+    private suspend fun readCoinsAmount(): String {
+        val coins = readUserDataUseCase.invoke()
+
+        _coinsAmount.value = coins
+
+        return coins
     }
 }
