a42c2f62c3e4feacb4521206d25340ef4c2d7cb0
Kazoroo <kazoroo1903@gmail.com>
2024-12-07 18:50:23 +0100

    Fix bug that user cannot watch second ad.

--- app/src/main/java/pl/kazoroo/dices/shop/domain/AdManager.kt
+++ app/src/main/java/pl/kazoroo/dices/shop/domain/AdManager.kt
@@ -3,8 +3,9 @@ package pl.kazoroo.dices.shop.domain
 import android.app.Activity
 import android.content.Context
 import android.util.Log
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.admanager.AdManagerAdRequest
 import com.google.android.gms.ads.rewarded.RewardedAd
 import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
@@ -13,14 +14,14 @@ object AdManager {
     private var rewardedAd: RewardedAd? = null
     val TAG = "AdManager"
 
-    fun setupAdMob(context: Context) {
+    fun loadRewardedAd(context: Context) {
         val adRequest = AdManagerAdRequest.Builder().build()
 
-        MobileAds.initialize(context)
         RewardedAd.load(
             context,
             "ca-app-pub-3940256099942544/5224354917",
-            adRequest, object : RewardedAdLoadCallback() {
+            adRequest,
+            object : RewardedAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
                     Log.d(TAG, "The ad failed to load - $adError")
                     rewardedAd = null
@@ -29,6 +30,19 @@ object AdManager {
                 override fun onAdLoaded(ad: RewardedAd) {
                     Log.d(TAG, "The ad has been loaded successfully")
                     rewardedAd = ad
+
+                    rewardedAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                        override fun onAdDismissedFullScreenContent() {
+                            Log.d(TAG, "Ad was dismissed.")
+                            if (rewardedAd == null) {
+                                loadRewardedAd(context)
+                            }
+                        }
+
+                        override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                            Log.e(TAG, "Ad failed to show $adError")
+                        }
+                    }
                 }
             }
         )
@@ -44,6 +58,8 @@ object AdManager {
 
                 onRewardEarned(rewardAmount.toString())
             }
+
+            loadRewardedAd(context)
         } ?: run {
             Log.d(TAG, "The rewarded ad wasn't ready yet.")
         }
